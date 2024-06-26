25d340e39af608cd4b45d28b4713a1d8ab8ec889
OoiSiangMing <siangming0520@gmail.com>
2024-06-26 21:14:31 +0800

    database

---
+++ public/js/register.js
@@ -0,0 +1,20 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAgZwA1oGjJnfGRgjWkIICfWhFELj8dxmU",
+    authDomain: "midkea-571f4.firebaseapp.com",
+    databaseURL: "https://midkea-571f4-default-rtdb.asia-southeast1.firebasedatabase.app/", // Updated URL
+    projectId: "midkea-571f4",
+    storageBucket: "midkea-571f4.appspot.com",
+    messagingSenderId: "371876454277",
+    appId: "1:371876454277:web:19b7e9ce993cfec2792eec"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
